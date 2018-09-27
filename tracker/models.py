16c76a786901ef8f126f2fa6c60f5b400dd91c97
Evan Miller <evanmiller@Evans-MacBook-Pro.local>
2018-09-26 23:39:17 -0400

    exercise api initial commit.

---
+++ tracker/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from rest_framework import serializers
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, GenericAPIView, DestroyAPIView
+from rest_framework import reverse, renderers
+
+
+# Create your models here.
